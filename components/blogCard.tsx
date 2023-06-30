e2d6cf279a8d29f17febe8fdef60ee7e051c1554
nawashiro <hooochiba@gmail.com>
2023-06-30 15:26:50 +0900

    ブログカードのurl表示をドメイン表示に変更

--- components/blogCard.tsx
+++ components/blogCard.tsx
@@ -2,6 +2,7 @@ import style from "../styles/BlogCard.module.css";
 import { Img } from "react-image";
 
 export default function Blogcard({ meta }) {
+  const domain = meta.url.match(/^https?:\/{2,}(.*?)(?:\/|\?|#|$)/)[1];
   return (
     <a href={meta.url} target="_brank" className={style.a}>
       <figure className={style.blogcard}>
@@ -15,7 +16,7 @@ export default function Blogcard({ meta }) {
           />
           <div className={style.title}>{meta.title}</div>
           <div className={style.description}>{meta.description}</div>
-          <div className={style.url}>{meta.url}</div>
+          <div className={style.url}>{domain}</div>
         </div>
       </figure>
     </a>
--- components/blogCardImitation.tsx
+++ components/blogCardImitation.tsx
@@ -2,6 +2,7 @@ import style from "../styles/BlogCard.module.css";
 import { Img } from "react-image";
 
 export default function BlogcardImitation({ url, title, description, image }) {
+  const domain = url.match(/^https?:\/{2,}(.*?)(?:\/|\?|#|$)/)[1];
   return (
     <a href={url} target="_brank" className={style.a}>
       <figure className={style.blogcard}>
@@ -9,7 +10,7 @@ export default function BlogcardImitation({ url, title, description, image }) {
           <img src={image} alt={title} width="100px" className={style.image} />
           <div className={style.title}>{title}</div>
           <div className={style.description}>{description}</div>
-          <div className={style.url}>{url}</div>
+          <div className={style.url}>{domain}</div>
         </div>
       </figure>
     </a>
